845195220683b644badfbb326c4f707fd9937177
ivan <ivan.khomenyuk@yahoo.com>
2024-07-23 16:35:44 +0300

    Added cart functionality

--- shop_frontend/src/Components/PaymentPage.js
+++ shop_frontend/src/Components/PaymentPage.js
@@ -70,19 +70,19 @@ function PaymentPage(props) {
                     <Paper elevation={3} sx={{ padding: 2 }}>
                         <List style={{ width: '100%' }}>
                             {(orderData && productsFromOrder) &&
-                                productsFromOrder.map((item, index) => {
+                                orderData.orderItems.map((item, index) => {
                                     if (!item) return null; // Check if item is undefined
-                                    const product = orderData.orderItems.find(
-                                        productFromOrder => productFromOrder.productId === item.id);
+                                    const product = productsFromOrder.find(
+                                        productFromOrder => productFromOrder.id === item.productId);
 
                                     if (!product) return null; // Check if product is undefined
 
                                     return (
                                         <React.Fragment key={index}>
                                             <CheckoutShortcut
-                                                item={item}
-                                                size={item.clothingSizes.find(size => size.id === product.clothingSizeId).name}
-                                                quantity={product.quantity}
+                                                item={product}
+                                                size={product.clothingSizes.find(size => size.id === item.clothingSizeId).name}
+                                                quantity={item.quantity}
                                             />
                                             {index < productsFromOrder.length - 1 && <Divider />}
                                         </React.Fragment>
